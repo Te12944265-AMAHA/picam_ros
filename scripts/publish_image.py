1df2a9911e71882721ee8d07f7b571bd9e1ce723
Te12944265-AMAHA <1603699457@qq.com>
2021-07-24 18:07:32 -0400

    works but slow

--- scripts/publish_image.py
+++ scripts/publish_image.py
@@ -16,7 +16,7 @@ import numpy as np
 class ImagePublisher:
 
   def __init__(self):
-    self.image_pub = rospy.Publisher("picamera/image",Image)
+    self.image_pub = rospy.Publisher("picamera/image",Image, queue_size=1)
 
     self.bridge = CvBridge()
 
@@ -30,14 +30,23 @@ def main(args):
   imgpub = ImagePublisher()
   rospy.init_node('image_publisher', anonymous=True)
   rate = rospy.Rate(15)
+  w = 2592
+  h = 1936
+  dw = 32.0
+  dh = 16.0
+  #wr = int(dw * np.ceil(w / dw))
+  #hr = int(dh * np.ceil(h / dh))
   with picamera.PiCamera() as camera:
-    camera.sensor_mode = 2 #2592x1944
+    #camera.sensor_mode = 2 #2592x1944
+    camera.resolution = (w, h)
     camera.framerate = 15
     time.sleep(2)
-    image = np.empty((2594, 1944, 3), dtype=np.uint8)
+    image = np.empty((h * w * 3,), dtype=np.uint8)
     while not rospy.is_shutdown():
       camera.capture(image, 'bgr')
-      imgpub.publish_img(image)
+      imager = image.reshape((h, w, 3))
+      #imagec = imager[:h, :w, :]
+      imgpub.publish_img(imager)
       rate.sleep()
   cv2.destroyAllWindows()
 
